1ae027a12adf5114cd4231e8d766445319450279
Lucas Duarte <lucasduarte_94@hotmail.com>
2021-08-27 22:36:38 -0300

    module completed without README file

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,21 @@
 import { SignInWithGithub } from '../SignInWithGithub';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../activeLink';
 
 export function Header() {
+    
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ignews" />
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active}>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                     <SignInWithGithub />
             </div>
